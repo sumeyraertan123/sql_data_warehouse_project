857ce176b66ad791870f4b8918d2553d0470b7d2
sumeyraertan123 <sumeyraertan123@gmail.com>
2025-12-21 20:28:21 +0300

    Add SQL script for data cleansing of CRM product info

---
+++ scripts/silver/data_cleansing/crm_prd_info/data_cleansing_crm_prd.sql
@@ -0,0 +1,26 @@
+INSERT INTO silver.crm_prd_info (
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start,
+	prd_end_dt
+)
+SELECT
+	prd_id,
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+	SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+	prd_nm,
+	ISNULL(prd_cost, 0) AS prd_cost,
+	CASE UPPER(TRIM(prd_line))
+		WHEN 'M' THEN 'Mauntain'
+		WHEN 'R' THEN 'Road'
+		WHEN 'S' THEN 'Other Sales'
+		WHEN 'T' THEN 'Touring'
+		ELSE 'n/a'
+	END AS prd_line,
+	CAST (prd_start AS DATE) AS prd_start,
+	CAST (LEAD(prd_start) OVER (PARTITION BY prd_key ORDER BY prd_start)-1 AS DATE) AS prd_end_dt
+FROM bronze.crm_prd_info
